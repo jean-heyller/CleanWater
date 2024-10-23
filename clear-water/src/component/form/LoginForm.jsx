07ed254713e447626af5abe2a0a3e9a7e29cd17c
christian.villegas_p <christian.villegas@pragma.com.co>
2024-10-22 21:24:29 -0500

    Se elimina una prop que no se usa

--- clear-water/src/component/form/LoginForm.jsx
+++ clear-water/src/component/form/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GoogleButton from '../button/GoogleButton';
 import Title from "../title/Title"
 
-const LoginForm = ( navigate ) => {
+const LoginForm = () => {
 
   return (
     
