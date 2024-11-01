ad5719e82dc2850cd1d92e5f5f038ec594e0f7f1
Santiago153 <santiago.nunez.ramirez@correounivalle.edu.co>
2024-10-31 21:09:52 -0500

    agregando texto a la pagina

--- clear-water/src/pages/recycling/Recycling.jsx
+++ clear-water/src/pages/recycling/Recycling.jsx
@@ -2,7 +2,7 @@ import { Canvas, useLoader } from '@react-three/fiber';
 import { useRef } from 'react';
 import React from 'react';
 import { TextureLoader, RepeatWrapping } from 'three';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Text } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
 
@@ -53,7 +53,7 @@ const CameraController = () => {
       minPolarAngle={Math.PI / 4}
       maxAzimuthAngle={Math.PI / 4}
       minAzimuthAngle={-Math.PI / 4}
-      maxDistance={20}
+      maxDistance={30}
       minDistance={5}
     />
   );
@@ -68,6 +68,50 @@ const PlantasModel = () => {
   const { scene } = useLoader(GLTFLoader, '/models-3d/plantas.glb');
   return <primitive object={scene} position={[0, 0, 0]} scale={[8, 8, 8]} castShadow />;
 };
+
+const OceanAcidificationText = () => {
+  const text = `La acidificación del océano es un fenómeno que ocurre 
+cuando el agua del mar absorbe dióxido de carbono (CO2) 
+de la atmósfera, lo que provoca una disminución en el pH 
+del agua. Este proceso afecta gravemente a los ecosistemas 
+marinos, ya que organismos como los corales y los moluscos 
+dependen de un ambiente más alcalino para formar sus 
+estructuras calcáreas. Con la reducción del pH, estos 
+organismos se debilitan, lo que altera la cadena alimentaria 
+y afecta a la biodiversidad marina. La acidificación es 
+una consecuencia directa del cambio climático y requiere 
+una acción urgente para mitigar su impacto en los océanos.`;
+
+  // Puedes ajustar estas posiciones según sea necesario
+  const positionX = -5; // Posición en el eje X
+  const positionY = 10;  // Posición en el eje Y
+  const positionZ = -50; // Posición en el eje Z
+
+  return (
+    <>
+      {/* Cuadrado grande detrás del texto */}
+      <mesh position={[positionX, 17, positionZ]}>
+        <planeGeometry args={[60, 29]} />
+        <meshBasicMaterial color="black" />
+      </mesh>
+      
+      {/* Texto en 3D */}
+      <Text
+        position={[positionX, 17, -45]}
+        fontSize={1.5}
+        color="white"
+        anchorX="center"
+        anchorY="middle"
+        lineHeight={1.2}
+      >
+        {text}
+      </Text>
+    </>
+  );
+};
+
+
+
 
 const Scene = () => {
   return (
@@ -91,6 +135,7 @@ const Scene = () => {
       <Floor />
       <FishModel />
       <PlantasModel />
+      <OceanAcidificationText />
       <CameraController />
     </Canvas>
   );
