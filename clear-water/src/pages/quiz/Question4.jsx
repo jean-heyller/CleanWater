2168e938060132c274917722d29679510a7a0d9b
blessed0314 <christian.daniel999@gmail.com>
2024-12-13 18:19:18 -0500

    Se terminan de configurar quices con estado

--- clear-water/src/pages/quiz/Question4.jsx
+++ clear-water/src/pages/quiz/Question4.jsx
@@ -1,43 +1,20 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import QuizModel from "../../component/quiz-model/QuizModel";
 import { QUESTION_4 } from "../../locales/water-shortage-text.json";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import UserDao from "../../daos/UserDao";
-import { useSelector } from "react-redux";
-import useAuthStore from "../../stores/use-auth-store";
-import { addQuiz } from "../../redux/QuizSlice";
+import Modal from "../../component/modal/Modal";
 
 const Question4 = () => {
   const { title, solution, errorMessage, correctMessage } = QUESTION_4;
   const [isCorrect, setIsCorrect] = useState(null);
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const { user } = useAuthStore();
- 
-  const data = useSelector((state) => state.quiz);
-
   const preguntaActual = 4;
 
   useEffect(() => {
     if (isCorrect !== null) {
       setModalIsOpen(true);
-      const quiz = {
-        question: {
-          isCorrect: isCorrect,
-          questionNumber: preguntaActual,
-        },
-        currentQuestion: currentQuestion,
-      };
-
     }
   }, [isCorrect]);
-
-  const goToNextQuestion = () => {
-    navigate("/question5"); // Cambia "/next-question" por la ruta de la siguiente pregunta
-  };
 
   return (
     <>
@@ -51,27 +28,13 @@ const Question4 = () => {
         <span className="text-white text-2xl">{title}</span>
       </div>
       {modalIsOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
-          <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-lg max-h-full overflow-y-auto">
-            {isCorrect ? (
-              <>
-                <span className="text-green-500 text-4xl">✔️</span>
-                <p className="text-green-500 text-xl">{correctMessage}</p>
-              </>
-            ) : (
-              <>
-                <span className="text-red-500 text-4xl">❌</span>
-                <p className="text-red-500 text-xl">{errorMessage}</p>
-              </>
-            )}
-            <button
-              onClick={goToNextQuestion}
-              className="mt-4 p-2 bg-blue-500 text-white rounded"
-            >
-              Siguiente Pregunta
-            </button>
-          </div>
-        </div>
+        <Modal
+          isCorrect={isCorrect}
+          onClose={() => setModalIsOpen(false)}
+          preguntaSiguiente={preguntaActual + 1}
+          errorMessage={errorMessage}
+          correctMessage={correctMessage}
+        />
       )}
     </>
   );
--- clear-water/src/pages/quiz/Question6.jsx
+++ clear-water/src/pages/quiz/Question6.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import QuizModel from "../../component/quiz-model/QuizModel";
 import { QUESTION_6 } from "../../locales/water-shortage-text.json";
-import { useNavigate } from "react-router-dom";
+import Modal from "../../component/modal/Modal";
 
 const Question6 = () => {
   const [isCorrect, setIsCorrect] = useState(null);
   const { title, solution, errorMessage, correctMessage } = QUESTION_6;
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  
-  const navigate = useNavigate();
+
   const preguntaActual = 6;
 
   useEffect(() => {
@@ -17,39 +16,25 @@ const Question6 = () => {
     }
   }, [isCorrect]);
 
-  const goToNextQuestion = () => {
-    navigate("/home"); // Cambia "/next-question" por la ruta de la siguiente pregunta
-  };
-
   return (
     <>
-      <QuizModel title="Pregunta 6" solution={solution} setIsCorrect={setIsCorrect}/>
+      <QuizModel
+        title="Pregunta 6"
+        solution={solution}
+        setIsCorrect={setIsCorrect}
+      />
       <div className="absolute top-0 left-0 w-full bg-black/85 text-white p-4 border-b border-gray-300 shadow-lg">
         <span className="text-blue-500 font-bold text-2xl">Pregunta 6: </span>
         <span className="text-white text-2xl">{title}</span>
       </div>
       {modalIsOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
-          <div className="bg-white p-6 rounded-lg shadow-lg text-center max-w-lg max-h-full overflow-y-auto">
-            {isCorrect ? (
-              <>
-                <span className="text-green-500 text-4xl">✔️</span>
-                <p className="text-green-500 text-xl">{correctMessage}</p>
-              </>
-            ) : (
-              <>
-                <span className="text-red-500 text-4xl">❌</span>
-                <p className="text-red-500 text-xl">{errorMessage}</p>
-              </>
-            )}
-            <button
-              onClick={goToNextQuestion}
-              className="mt-4 p-2 bg-blue-500 text-white rounded"
-            >
-              Ir al home
-            </button>
-          </div>
-        </div>
+        <Modal
+          isCorrect={isCorrect}
+          onClose={() => setModalIsOpen(false)}
+          preguntaSiguiente={preguntaActual + 1}
+          errorMessage={errorMessage}
+          correctMessage={correctMessage}
+        />
       )}
     </>
   );
